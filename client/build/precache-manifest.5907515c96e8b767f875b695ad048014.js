3dc3a3f236b48e52366b308073175302e3df80fc
Douglas Chan <douglas@mediushealth.org>
2019-09-16 16:26:54 +1000

    $COMMIT_MSG

---
+++ client/build/precache-manifest.5907515c96e8b767f875b695ad048014.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f03de950c6883522a111497576803ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83895d63d817b243ded5",
+    "url": "/static/css/main.223fcd03.chunk.css"
+  },
+  {
+    "revision": "61a2082a5a1ab224270d",
+    "url": "/static/js/2.75b3519e.chunk.js"
+  },
+  {
+    "revision": "83895d63d817b243ded5",
+    "url": "/static/js/main.e687c046.chunk.js"
+  },
+  {
+    "revision": "df35b418445d95524673",
+    "url": "/static/js/runtime~main.488beccc.js"
+  }
+]);
